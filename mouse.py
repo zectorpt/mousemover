54798322ae411633487798ce73d6c74f32700f4a
José Almeida <josemedeirosdealmeida@gmail.com>
2020-04-15 16:34:10 +0100

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -1,6 +1,6 @@
 import pyautogui, time
 print("Exiting by CTRL+C.\n")
-print("\n\n\nV 0.1\n")
+print("\n\n\nhttps://github.com/zectorpt/mousemover \nV 0.1\n")
 i=0
 while i <= 10:
 	pyautogui.move(2, 2)
