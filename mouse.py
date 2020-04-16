08a3753d52b7fcf7b66e1ebbc9c3f4eee080b493
José Almeida <josemedeirosdealmeida@gmail.com>
2020-04-16 09:02:35 +0100

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -1,8 +1,12 @@
-import pyautogui, time
+import pyautogui, time, sys
+from datetime import datetime
+pyautogui.FAILSAFE = False
 print("Exiting by CTRL+C.\n")
-print("\n\n\nhttps://github.com/zectorpt/mousemover \nV 0.1\n")
+print("\n\n\nhttps://github.com/zectorpt/mousemover \nV 0.3\n")
 i=0
 while i <= 10:
-	pyautogui.move(2, 2)
-	pyautogui.move(-2, -2)
+	pyautogui.move(5, 5)
+	pyautogui.move(-5, -5)
+	pyautogui.press("shift")
+	print("Movement made at {}".format(datetime.now().time()))
 	time.sleep (5)
